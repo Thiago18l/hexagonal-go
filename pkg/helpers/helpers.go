461d069280b6eb9c01e02720065479118d73f902
Thiago L <thiago.lopes.dev@gmail.com>
2021-08-27 14:22:05 -0300

    feat: add helpers

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"net/http"
+
+	"github.com/matiasvarela/errors"
+	"github.com/thiago18l/hexagonal-go/pkg/apperrors"
+)
+
+type ApiError struct {
+	Status  int         `json:"status"`
+	Code    string      `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func New(err error) (int, ApiError) {
+	switch errors.Code(err) {
+	case errors.Code(apperrors.NotFound):
+		return http.StatusNotFound, ApiError{Status: http.StatusNotFound, Code: errors.Code(apperrors.NotFound), Message: err.Error(), Data: errors.Data(err)}
+	case errors.Code(apperrors.InvalidInput):
+		return http.StatusBadRequest, ApiError{Status: http.StatusBadRequest, Code: errors.Code(apperrors.InvalidInput), Message: err.Error(), Data: errors.Data(err)}
+	default:
+		return http.StatusInternalServerError, ApiError{Status: http.StatusInternalServerError, Code: errors.Code(apperrors.Internal), Message: err.Error(), Data: errors.Data(err)}
+	}
+}
